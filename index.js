71430ebca956914357293afe482778ff7f99212f
Sheikhmsip <sheikhmsip@gmail.com>
2023-07-24 08:59:35 +0600

    start project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('College is sitting')
+  })
+  
+  app.listen(port, () => {
+    console.log(`College is sitting on port ${port}`)
+  })
