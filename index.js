5a01807cc64a2c199cb4f8629d37808310c4739d
Sheikhmsip <sheikhmsip@gmail.com>
2023-07-24 23:23:49 +0600

    added server

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-
-
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('College is sitting')
+    res.send('College is running')
   })
   
   app.listen(port, () => {
-    console.log(`College is sitting on port ${port}`)
+    console.log(`College is running on port ${port}`)
   })
